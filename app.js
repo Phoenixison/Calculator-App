fd429bd7d31e0cd07397321790894f932e9918cb
Gautam Kashyap <73703174+Phoenixison@users.noreply.github.com>
2024-09-10 16:46:53 +0530

    Updated functioning of calculator with UI

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 let btngroup = document.querySelectorAll('.btns')
 let p = document.querySelector('p')
+let display = document.getElementById('display')
 
-btngroup.forEach(
-    (btn) => {
-        btn.addEventListener('click', () => {
-            p.innerText += btn.innerText
-        })
+function addToScreen(a) {
+    display.value += a;
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
     }
-)
+    catch(e) {
+        display.value = "Error";
+        alert("You have entered wrong value !");
+    }
+}
+
+function clearScreen() {
+    display.value = "";
+}
+
